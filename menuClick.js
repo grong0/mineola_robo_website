b7f9461ecb1b4587d88c22fdf69d6196bdd3e4fc
grongo59100 <garrett59100@gmail.com>
2022-10-17 17:16:00 -0400

    finished the welcome screen

--- menuClick.js
+++ menuClick.js
@@ -1,29 +1,59 @@
 menu = document.getElementById("menu");
 wrapper = document.getElementById("wrapper");
-var toggle = 0;
-var firstTime = True;
+menu_button = document.getElementById("menu_button");
+icon = document.getElementById("menu_icon");
+logo = document.getElementById("logo");
+home = document.getElementById("home");
+contact = document.getElementById("contact");
+about = document.getElementById("about");
+code = document.getElementById("code");
 
-function pushAway() {
-    if (!firstTime) {
-        wrapper.classList.remove("cameBack");
-    }
-    else {
-        firstTime = False;
-    }
-    wrapper.classList.add("pushedAway");
-}
-function comeBack() {
-    wrapper.classList.remove("pushedAway");
-    wrapper.classList.add("cameBack");
-}
+// var toggle = 0;
+// var firstTime = True;
+
+// function pushAway() {
+//     if (!firstTime) {
+//         wrapper.classList.remove("cameBack");
+//     }
+//     else {
+//         firstTime = False;
+//     }
+//     wrapper.classList.add("pushedAway");
+// }
+// function comeBack() {
+//     wrapper.classList.remove("pushedAway");
+//     wrapper.classList.add("cameBack");
+// }
+
+// function clicked(){
+//     if(toggle == 0){
+//         pushAway();
+//         toggle = 1;
+//     }
+//     else if(toggle == 1){
+//         comeBack();
+//         toggle = 0;
+//     }
+// }
 
 function clicked(){
-    if(toggle == 0){
-        pushAway();
-        toggle = 1;
-    }
-    else if(toggle == 1){
-        comeBack();
-        toggle = 0;
-    }
+    wrapper.classList.toggle('pushed');
+    wrapper.classList.toggle('pulled');
+    menu_button.classList.toggle('clicked');
+    menu_button.classList.toggle('idle');
+    icon.classList.toggle('clicked');
+    icon.classList.toggle('idle');
+    logo.classList.toggle('btnActive');
+    logo.classList.toggle('btnDeactive');
+
+    home.classList.toggle('in');
+    home.classList.toggle('out');
+    about.classList.toggle('in');
+    about.classList.toggle('out');
+    contact.classList.toggle('in');
+    contact.classList.toggle('out');
+    code.classList.toggle('in');
+    code.classList.toggle('out');
+
+
 }
